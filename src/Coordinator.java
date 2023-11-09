dbc873e7a62f5dccf27317e6c073f477c445dd89
ikomers <komers.for@gmail.com>
2023-11-09 11:49:40 +0300

    Add HashMap countEntitiesInGameField()

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -6,15 +6,28 @@ import src.IslandLivingObject.IslandEntityType;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Coordinator {
     IslandField islandField = IslandField.getInstance();
     IslandEntityType array[] = IslandEntityType.values();
+    private boolean isGameFieldExsist;
 
-    // TODO иногда (~1 из 10 раз) падает на ошибке, думаю это связано с тем, что животные не успевают проинициализироваться и "встать" на клетку
+    // TODO добавить статиску животных по всему острову
     public void start() {
         List[][] gameField = IslandField.getGameField();
+
+        //даем проинициализироваться всем обектам
+        if (!isGameFieldExsist) {
+            try {
+                Thread.sleep(500);
+                isGameFieldExsist = true;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         for (int i = 0; i < islandField.getNumColumns(); i++) {
             for (int j = 0; j < islandField.getNumRows(); j++) {
                 System.out.println("\n" + "Counts in cell (" + i + ", " + j + "):" + "\n");
@@ -24,6 +37,8 @@ public class Coordinator {
                 }
             }
         }
+
+        System.out.println(countEntitiesInGameField(islandField));
     }
 
     private long countEntitiesInCell(List<IslandEntity> entities, IslandEntityType type) {
@@ -32,5 +47,22 @@ public class Coordinator {
                 .count();
     }
 
+    private HashMap countEntitiesInGameField(IslandField field) {
+        HashMap<IslandEntityType, Integer> countOfEntityInGameField = new HashMap<>();
 
+        for (IslandEntityType type : array) {
+            countOfEntityInGameField.put(type, 0);
+        }
+
+        for (int i = 0; i < IslandField.getInstance().getNumRows() - 1; i++) {
+            for (int j = 0; j < IslandField.getInstance().getNumRows() - 1; j++) {
+                for (IslandEntityType type : array) {
+                    long entitiesInCell = countEntitiesInCell(IslandField.getGameField()[i][j], type);
+                    int entitiesInMap = countOfEntityInGameField.get(type);
+                    countOfEntityInGameField.put(type, (int) (entitiesInMap + entitiesInCell));
+                }
+            }
+        }
+        return countOfEntityInGameField;
+    }
 }

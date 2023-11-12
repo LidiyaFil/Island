e50aaffeb667a3b8f768e66b8a281a799fa92c64
ikomers <komers.for@gmail.com>
2023-11-12 16:37:11 +0300

    refactor whole eat()

--- src/Actions/Servicies/NutritionService.java
+++ src/Actions/Servicies/NutritionService.java
@@ -4,8 +4,10 @@ import src.IslandLivingObject.Animals.AbstractAnimal;
 import src.IslandLivingObject.Animals.Herbivorous.Herbivorous;
 import src.IslandLivingObject.Animals.Predators.Predators;
 import src.IslandLivingObject.IslandEntity;
+import src.IslandLivingObject.IslandEntityType;
 import src.IslandLivingObject.Plants.AbstractPlant;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -31,30 +33,10 @@ public class NutritionService {
 
     public boolean tryToEat(AbstractAnimal eating, IslandEntity lunch) {
         int chance = ThreadLocalRandom.current().nextInt(100);
-        return chance >= eating.getEdibleSpecies().get(lunch.getType());
+        if (eating.getEdibleSpecies().containsKey(lunch.getType())) {
+            return chance >= eating.getEdibleSpecies().get(lunch.getType());
+        } else {
+            return false;
+        }
     }
 }
-
-   /* private void feedPredator(Predators predator, List<IslandEntity> entities, AbstractAnimal abstractAnimal) {
-        entities.stream()
-                .filter(lunch -> lunch instanceof AbstractAnimal && predator.getEdibleSpecies().containsKey(lunch.getType()))
-                .forEach(lunch -> {
-                    boolean result = tryToEat(predator, lunch);
-                    if (result) {
-                        double eaterSaturation = predator.getSaturation();
-                        double lunchWeight = lunch.getType().getWeight();
-                        abstractAnimal.setSaturation(abstractAnimal.getSaturation() + Math.min(eaterSaturation, lunchWeight));
-//                        System.out.println("съели" + lunch);
-                        entities.remove(lunch);
-                    }
-                });
-    }
-
-    private void feedHerbivorous(Herbivorous herbivorous, List<IslandEntity> entities) {
-        entities.stream()
-                .filter(lunch -> lunch instanceof AbstractPlant)
-                .forEach(lunch -> {
-                    herbivorous.setSaturation(lunch.getType().getWeight());
-                    entities.remove(lunch);
-                });
-    }*/
